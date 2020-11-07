6f04e38260f01ccf74dde74d995c5c94d5f619e3
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-07 15:28:58 -0800

    post req

---
+++ routes/api.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const Transaction = require("models/workout.js");
+
+// posting workout to the api
+router.post("/api/workout", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+//
+
+// router.post("/api/workout/bulk", ({ body }, res) => {
+//   Transaction.insertMany(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
