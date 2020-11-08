88ac4dd90eea24698e22424cb307b32de45ad735
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-11-07 18:20:46 -0800

    get api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
-const Transaction = require("models/workout.js");
+const Workouts = require("models/workout.js");
+
+// getting info from api
+router.get("/api/workout", (req, res) => {
+    Workouts.find({})
+    .then(db => { res.json(db); })
+    .catch(err => {res.json(err);});
+});
 
 // posting workout to the api
 router.post("/api/workout", ({ body }, res) => {
-  Transaction.create(body)
+  Workouts.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
@@ -13,6 +20,9 @@ router.post("/api/workout", ({ body }, res) => {
 });
 
 //
+
+// posting to stats page
+
 
 // router.post("/api/workout/bulk", ({ body }, res) => {
 //   Transaction.insertMany(body)
